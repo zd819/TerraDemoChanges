d2bcc18b572f6c02362932875649ce099347bc53
DjDarkz <zd819@ic.ac.uk>
2022-05-03 15:46:15 +0100

    Current Login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,23 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Dashboard from './components/Dashboard/Dashboard';
 import Preferences from './components/Preferences/Preferences';
 import Login from './components/Login/Login';
+import useToken from './useToken';
+import logo from './misc/T_only.svg';
 
 function App() {
-  const [token, setToken] = useState();
+  const { token, setToken } = useToken();
+
   if(!token) {
     return <Login setToken={setToken} />
   }
 
   return (
     <div className="wrapper">
-      <h1>Application</h1>
+      <h1>Terra Terra</h1>
+      <img src={logo} className="App-logo" alt="logo" />
       <BrowserRouter>
         <Routes>
           <Route path='/Dashboard' element={<Dashboard />} />
