6f3295f88d4e2c7ac5b04515256e0af7055c38f6
DjDarkz <zd819@ic.ac.uk>
2022-05-04 15:33:26 +0100

    Trying to retrieve sessionID and use to connect to Terra Widget

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,21 +4,27 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Dashboard from './components/Dashboard/Dashboard';
 import Preferences from './components/Preferences/Preferences';
 import Login from './components/Login/Login';
+import Iframe from './components/Widget/Widget.js'
+import PostTerra from './components/Widget/PostTerra.js'
 import useToken from './useToken';
 import logo from './misc/T_only.svg';
 
-
+//Either in the Widget.js or before we set shown to true, we must request
+//SessionID from the backend and append to terraURL
 
 
 
 function App() {
 
-  const openWidget = async () => {
-    <iframe src="https://www.google.com/webhp?igu=1"></iframe>
-  };
-  
+  const [shown, setShown] = React.useState(false)
+  const [showID, setID] = React.useState(false)
+  const verURL = 'https://api.tryterra.co/v2/auth/generateWidgetSession'
+  const authURL = "https://api.tryterra.co/v2/auth/authenticateUser"
+  const terraURL = "https://widget.tryterra.co/v2/"
 
   const { token, setToken } = useToken();
+
+  const { iD, setiD } = PostTerra();
 
   if(!token) {
     return <Login setToken={setToken} />
@@ -34,9 +40,14 @@ function App() {
           <Route path='/Preferences' element={<Preferences />} />
         </Routes>
       </BrowserRouter>
-      <button className="Terra-link" onClick={openWidget}>
+      <button className="Terra-link" onClick={() => setShown(!shown)}>
           Connect to Terra Terra
       </button>
+      {shown ?  <Iframe className='Widget' source= {verURL} /> : null}
+      <button className="Session-iD" onClick={() => setID(!showID)}>
+          Get Session ID
+      </button>
+      {shown ?  <h2> {iD}</h2> : null}
     </div>
   );
 }
