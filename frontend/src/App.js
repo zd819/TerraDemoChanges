43be79f10d084a088ace1a2701405dd135ade077
DjDarkz <zd819@ic.ac.uk>
2022-05-09 16:57:41 +0100

    Sidebar in front end

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,10 +4,10 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Dashboard from './components/Dashboard/Dashboard';
 import Preferences from './components/Preferences/Preferences';
 import Login from './components/Login/Login';
+import Sidebar from './components/Sidebar/Sidebar.js';
 import Iframe from './components/Widget/IFrame.js'
 import useToken from './useToken';
 import logo from './misc/T_only.svg';
-import Popup from './components/Widget/Popup.js'
 
 //Either in the Widget.js or before we set shown to true, we must request
 //SessionID from the backend and append to terraURL
@@ -27,25 +27,32 @@ const handleClick = () => {
 
 function App() {
   const { token, setToken } = useToken();
+  const { dash, setDash } = useState(true);
+
+  console.log("VALUE OF DASH IS", dash)
   if(!token) {
     return <Login setToken={setToken} />
   }
-
   return (
     <div className="wrapper">
       <h1>Terra Terra</h1>
-      <img src={logo} className="App-logo" alt="logo" />
       <BrowserRouter>
+      <div className="Temp">
+        <Sidebar className="Sidebar" />
+        <img src={logo} className="App-logo" alt="logo" />
+      </div>
         <Routes>
           <Route path="/Dashboard" element={<Dashboard />} />
           <Route path="/Preferences" element={<Preferences />} />
         </Routes>
+        {!dash ? <button className="Terra-link" onClick={() => setDash("true")}>
+          Connect to Terra Terra {dash}
+        </button> : null}        
       </BrowserRouter>
-      <button className="Terra-link" onClick={handleClick}>
-          Connect to Terra Terra
-      </button>
     </div>
-  );
-}
+  )
+  
+  
+};
 
 export default App;
--- frontend/src/components/Login/Login.js
+++ frontend/src/components/Login/Login.js
@@ -36,10 +36,12 @@ export default function Login({ setToken }) {
           <p>Username</p>
           <input type="text" onChange={e => setUserName(e.target.value)}/>
         </label>
-        <label>
-          <p>Password</p>
-          <input type="password" onChange={e => setPassword(e.target.value)}/>
-        </label>
+        <div>
+          <label>
+            <p>Password</p>
+            <input type="password" onChange={e => setPassword(e.target.value)}/>
+          </label>
+        </div>
         <div>
           <button type="submit">Submit</button>
         </div>
