b492dc0b91b467f8efd365c13a2e5f0f22831d54
my5918 <my5918@imperial.ac.uk>
2022-05-23 18:33:02 +0100

    login page

--- frontend/src/partials/dashboard/WelcomeBanner.jsx
+++ frontend/src/partials/dashboard/WelcomeBanner.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function WelcomeBanner() {
   return (
-    <div className="relative bg-indigo-200 p-4 sm:p-6 rounded-sm overflow-hidden mb-8">
+    <div className="relative bg-blue-200 p-4 sm:p-6 rounded-sm overflow-hidden mb-8">
 
       {/* Background illustration */}
       <div className="absolute right-0 top-0 -mt-4 mr-16 pointer-events-none hidden xl:block" aria-hidden="true">
@@ -10,12 +10,12 @@ function WelcomeBanner() {
           <defs>
            
             <linearGradient x1="50%" y1="0%" x2="50%" y2="100%" id="welcome-b">
-              <stop stopColor="#A5B4FC" offset="0%" />
-              <stop stopColor="#818CF8" offset="100%" />
+              <stop stopColor="#eff6ff" offset="0%" />
+              <stop stopColor="#dbeafe" offset="100%" />
             </linearGradient>
             <linearGradient x1="50%" y1="24.537%" x2="50%" y2="100%" id="welcome-c">
-              <stop stopColor="#4338CA" offset="0%" />
-              <stop stopColor="#6366F1" stopOpacity="0" offset="100%" />
+              <stop stopColor="#1d4ed8" offset="0%" />
+              <stop stopColor="#3b82f6" stopOpacity="0" offset="100%" />
             </linearGradient>
           </defs>
           <g fill="none" fillRule="evenodd">
