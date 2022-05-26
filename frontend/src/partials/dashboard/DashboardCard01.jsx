8ab3422c1ce86a368a38441b5d26d1a0606c8a55
DjDarkz <zd819@ic.ac.uk>
2022-05-26 18:55:54 +0100

    Switched Nutrition and Health cards around

--- frontend/src/partials/dashboard/DashboardCard01.jsx
+++ frontend/src/partials/dashboard/DashboardCard01.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import LineChart from '../../charts/LineChart01';
-import Icon from '../../images/icon-01.svg';
+import Icon from '../../images/icon-03.svg';
 import EditMenu from '../EditMenu';
 import PollData from '../../components/Dashboard/PollData';
 // import getData from '../../components/Dashboard/testData.js'
@@ -189,12 +189,12 @@ function DashboardCard01() {
           </EditMenu>
         </header>
         
-        <h2 className="text-lg font-semibold text-slate-800 mb-2">Health</h2>
-        <div className="text-xs font-semibold text-slate-400 uppercase mb-1">Your Health Data Analysis</div>
+        <h2 className="text-lg font-semibold text-slate-800 mb-2">Nutrition</h2>
+        <div className="text-xs font-semibold text-slate-400 uppercase mb-1">Your Nutrition Data Analysis</div>
         <div className="flex items-start">
           <div className="text-3xl font-bold text-slate-800 mr-2">3 Months Ago</div>
           <div className={'text-sm font-semibold text-white px-1.5 rounded-full ' + (calorieOver ? 'bg-yellow-500' : 'bg-green-500')}>
-            {calorieOver? Over : Under};
+            {calorieOver ? Over : Under};
             {console.log(green)}
           </div>
         </div>
--- frontend/src/partials/dashboard/DashboardCard03.jsx
+++ frontend/src/partials/dashboard/DashboardCard03.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import LineChart from '../../charts/LineChart01';
-import Icon from '../../images/icon-03.svg';
+import Icon from '../../images/icon-01.svg';
 import EditMenu from '../EditMenu';
 
 // Import utilities
@@ -78,8 +78,8 @@ function DashboardCard03() {
             </li>
           </EditMenu>
         </header>
-        <h2 className="text-lg font-semibold text-slate-800 mb-2">Nutrition</h2>
-        <div className="text-xs font-semibold text-slate-400 uppercase mb-1">Your Nutrition Data Analysis</div>
+        <h2 className="text-lg font-semibold text-slate-800 mb-2">Health</h2>
+        <div className="text-xs font-semibold text-slate-400 uppercase mb-1">Your Health Data Analysis</div>
         <div className="flex items-start">
           <div className="text-3xl font-bold text-slate-800 mr-2">Past 3 Months</div>
           <div className="text-sm font-semibold text-white px-1.5 bg-green-500 rounded-full">+49%</div>
