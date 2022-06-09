8b93fbad76623e2660a1a3b99f694940a8fb4e6c
DjDarkz <zd819@ic.ac.uk>
2022-06-09 19:17:05 +0100

    Getting result from promise fulfilled

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import {
   Routes,
@@ -6,7 +6,9 @@ import {
   useLocation 
 } from 'react-router-dom';
 
-import GenerateID from './components/User/UserID.js';
+import {sha1,sha256,sha384,sha512} from 'crypto-hash';
+
+// import GenerateID from './components/User/UserID.js';
 
 // Import pages
 import App2 from './App2.js';
@@ -22,14 +24,22 @@ function DashboardApp() {
   //   window.scroll({ top: 0 })
   //   document.querySelector('html').style.scrollBehavior = ''
   // }, [location.pathname]); // triggered on route change
-
+  const GenerateID = async () => {
+    const [userID, setUserID] = useState('');
+    const current = new Date().toLocaleString();
+    const result = JSON.stringify(await sha256(current));
+    console.log("User ID Unique Result is : ", result);
+    // console.log("useState ID : ", userID);
+    return result;
+  };
   var newID = GenerateID();
+  console.log("1111111 : ", newID);
   return (
     <BrowserRouter>
       <Routes>
         <Route exact path="/*" element={<App2 />} />
         <Route exact path="/Analytics" element={<AnalyticsMain />}/>
-        <Route exact path="/Dashboard" element={<DashboardMoz id={newID} />} />
+        <Route exact path="/Dashboard" element={<DashboardMoz id={newID} />}/>
       </Routes>
     </BrowserRouter>
   );
--- frontend/src/components/User/UserID.js
+++ frontend/src/components/User/UserID.js
@@ -1,14 +1,12 @@
 import {sha1,sha256,sha384,sha512} from 'crypto-hash';
 import React, { useEffect, useState } from 'react';
 
-async function GenerateID() {
-    const [userID, setUserID] = useState("");
+const GenerateID = async () => {
+    const [userID, setUserID] = useState('');
     const current = new Date().toLocaleString();
-    const test = "";
-    const result = JSON.stringify(await sha256(current).then((res => setUserID(res))));
+    const result = JSON.stringify(await sha256(current));
     console.log("User ID Unique Result is : ", result);
-    console.log("Testing promise : ", test);
     console.log("useState ID : ", userID);
-    return userID;
+    return result;
 };
 export default GenerateID;
