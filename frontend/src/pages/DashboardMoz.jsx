ebac429dd2623a650b9d705bed8d6b53225aad21
DjDarkz <zd819@ic.ac.uk>
2022-06-27 18:43:53 +0100

    ID flow

--- frontend/src/pages/DashboardMoz.jsx
+++ frontend/src/pages/DashboardMoz.jsx
@@ -140,7 +140,7 @@ class DashboardMoz extends React.Component {
     <div className="flex h-screen overflow-hidden bg-blue-50">
       
       {/* Sidebar */}
-      <Sidebar sidebarOpen={this.state.sidebarOpen} setSidebarOpen={this.setSidebarOpen} />
+      <Sidebar id={this.props.id} sidebarOpen={this.state.sidebarOpen} setSidebarOpen={this.setSidebarOpen} />
 
       {/* Content area */}
       <div className="relative flex flex-col flex-1 overflow-y-auto overflow-x-hidden">
